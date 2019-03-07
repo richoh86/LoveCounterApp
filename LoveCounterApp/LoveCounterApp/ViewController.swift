509e5f8fb9f3e134207a3076f2328b78396c4ad6
richoh86 <wsoh1986@gmail.com>
2019-03-07 18:39:13 +0900

    LoveCounterApp UI Start

--- LoveCounterApp/LoveCounterApp/ViewController.swift
+++ LoveCounterApp/LoveCounterApp/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var blackBackGroundView: UIView!
+    
+    let shapeLayer = CAShapeLayer()
+    let textView = UITextView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        blackBackGroundView.alpha = 0.4
         
+        let center = view.center
+        let circlarPathB = UIBezierPath(arcCenter: .zero, radius: 120, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        
+        shapeLayer.path = circlarPathB.cgPath
+        shapeLayer.strokeColor = UIColor.white.cgColor
+        shapeLayer.lineWidth = 10
+        shapeLayer.fillColor = .none
+        shapeLayer.position = center
+        
+        textView.center = center
+        textView.bounds.size = CGSize(width: 100, height: 87)
+        textView.text = "1일"
+        textView.textColor = UIColor.white
+        textView.textAlignment = .center
+        textView.backgroundColor = .none
+        textView.font = UIFont.boldSystemFont(ofSize: 50)
+        
+        view.layer.addSublayer(shapeLayer)
+        view.addSubview(textView)
         
     }
+    
+    // status bar -> white color
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
 }
 
