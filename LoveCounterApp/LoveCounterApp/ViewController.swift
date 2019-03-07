25fcdae956db23d250ba628a1f70a7012c8ad507
richoh86 <wsoh1986@gmail.com>
2019-03-07 16:05:04 +0900

    start lovecounterapp prj

---
+++ LoveCounterApp/LoveCounterApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoveCounterApp
+//
+//  Created by richard oh on 07/03/2019.
+//  Copyright © 2019 richard oh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
