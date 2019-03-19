e37259c5b9b4b0269533e9c1f218cf19b3a236ee
richoh86 <wsoh1986@gmail.com>
2019-03-19 14:53:48 +0900

    git udpate

--- LoveCounterApp/LoveCounterApp/AppDelegate.swift
+++ LoveCounterApp/LoveCounterApp/AppDelegate.swift
@@ -11,8 +11,8 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    // UIWindow에서 모든 VC를 관리하고 있다 참고할 것.
     var window: UIWindow?
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
